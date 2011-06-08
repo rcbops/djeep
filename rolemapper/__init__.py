692c0f5c3864d306424ae24f295678270e400948
Ron Pedde <ron@pedde.com>
2011-06-08 16:53:19 -0500

    Add rudimentary editing for key/values hosts

--- rolemapper/__init__.py
+++ rolemapper/__init__.py
@@ -3,5 +3,6 @@ from flaskext import sqlalchemy
 
 app = flask.Flask(__name__)
 app.config.from_object('rolemapper.config')
+app.secret_key = "supersecretsecretkey"
 
 db = sqlalchemy.SQLAlchemy(app)
--- rolemapper/views.py
+++ rolemapper/views.py
@@ -5,10 +5,13 @@ from rolemapper import models
 
 
 @app.route('/', methods=['GET'])
-def hello():
-    hw = models.HardwareInfo.query.all()
-    return '<br/>'.join([x.mac_address for x in hw])
+def html_default():
+    return flask.render_template('default.html')    
 
+@app.route('/hardware', methods=['GET'])
+def html_show_hardware():
+    hw = models.HardwareInfo.query.all()
+    return flask.render_template('show_hardware.html', hw=hw)
 
 @app.route('/hardware', methods=['POST'])
 def create():
@@ -21,7 +24,8 @@ def create():
 
 @app.route('/form', methods=['GET'])
 def form():
-    return flask.render_template('form.html', postback=flask.url_for('create'))
+    return flask.render_template('form.html',
+                                 postback=flask.url_for('create'))
 
 @app.route('/chef_key', methods=['POST'])
 # curl -H "Content-type: application/octet-stream" -XPOST --data-binary \
@@ -60,15 +64,65 @@ def generate_preseed(host_id):
     # hardware info should really be host info
     hw = models.HardwareInfo.query.get_or_404(host_id)
     
-    host_dist = "maverick"
-    host_arch = "amd64"
+    host_dist = 'maverick'
+    host_arch = 'amd64'
 
     kvpairs = models.TemplateVars.query.all()
     site = dict(zip([x.key for x in kvpairs], [x.value for x in kvpairs]))
 
-    template_file = "preseed/%s-%s-preseed.txt" % (host_dist, host_arch)
+    template_file = 'preseed/%s-%s-preseed.txt' % (host_dist, host_arch)
     return flask.render_template(template_file, host=hw, site=site)
     
+# Key/value pair management (web based)
+@app.route('/keys/', methods=['GET'])
+def html_show_keys():
+    kvpairs = models.TemplateVars.query.all()
+    site = dict(zip([x.key for x in kvpairs], [x.value for x in kvpairs]))
+    return flask.render_template('show_keys.html', site=site)
 
+@app.route('/keys/delete/<key>', methods=['GET'])
+def html_delete_key(key):
+    tv = models.TemplateVars.query.filter(
+            models.TemplateVars.key == key).first_or_404()
+
+    db.session.delete(tv)
+    db.session.commit()
+
+    flask.flash('deleted')
+    return flask.redirect(flask.url_for('html_show_keys'));
+
+@app.route('/keys/edit/<key>', methods=['GET'])
+def html_edit_key(key):
+    tv = models.TemplateVars.query.filter(
+            models.TemplateVars.key == key).first_or_404()
+    return flask.render_template('html_edit_key.html', input = { 'key': tv.key,
+                                                                 'value': tv.value })
     
+@app.route('/keys/edit/<key>', methods=['POST'])
+def html_post_edit_key(key):
+    req = flask.request
+    tv = models.TemplateVars.query.filter(
+            models.TemplateVars.key == req.form['key']).first()
+
+    action = 'Edited'
+    if tv == None:
+        tv = models.TemplateVars()
+        tv.key = req.form['key']
+        tv.value = req.form['value']
+        action = 'Created'
+    else:
+        tv.value = req.form['value']
+
+    rv = models.commit(tv)
+    flask.flash('%s key' % (action, ))
+    return flask.redirect(flask.url_for('html_show_keys'));
     
+@app.route('/keys/add/', methods=['GET'])
+def html_add_key():
+    return flask.render_template('html_edit_key.html', input = { 'key': '',
+                                                                 'value': '' })
+
+@app.route('/keys/add/', methods=['POST'])
+def html_post_add_key():
+    return html_post_edit_key(None)
+
