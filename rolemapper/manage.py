6399e442c6c44e21c6a43f099609aa27a2de563e
Will Kelly <the.william.kelly@gmail.com>
2011-06-21 17:31:38 -0500

    changed manage.py

--- rolemapper/manage.py
+++ rolemapper/manage.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python
 from migrate.versioning.shell import main
-main(url='sqlite:///../webapp.db', debug='False', repository='migration/')
+main(url='sqlite:///../webapp.db', debug='False', repository='rolemapper/migration/')
