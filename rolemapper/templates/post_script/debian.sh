3ef72d844ed5ba7e07a3a7c70efe0e146016b438
Will Kelly <the.william.kelly@gmail.com>
2011-06-23 17:18:24 -0500

    Set full hostname in /etc/hostname

--- rolemapper/templates/post_script/debian.sh
+++ rolemapper/templates/post_script/debian.sh
@@ -7,5 +7,6 @@ if [ -s /root/install.sh ]; then
     sed -i /etc/rc.local -e 's_exit 0_/root/install.sh_'
 fi
 
+echo "{{ host.hostname }}" > /etc/hostname
 curl -H 'Content-Type: application/json' http://{{ site.webservice_host }}:{{ site.webservice_port }}/admin/hardware/edit/{{ host.id }} -d'{ "kick_id": 1 }'
 
