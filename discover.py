2b16acdc6b86953a349210bf909442f57197691e
Will Kelly <the.william.kelly@gmail.com>
2011-06-22 22:21:05 -0500

    might talk to server now

--- discover.py
+++ discover.py
@@ -10,42 +10,43 @@ try:
 except ImportError:
     import simplejson as json
 
-hardware_cmd = ["/usr/bin/lshw", "-json"]
-cmdline_file = "/proc/cmdline"
+def discover():
+    hardware_cmd = ["/usr/bin/lshw", "-json"]
+    cmdline_file = "/proc/cmdline"
 
-hardware_info = subprocess.check_output(hardware_cmd, stderr=subprocess.PIPE)
-args = [ x.split("=",1) for x in 
-    open(cmdline_file, "r").readline().strip().split()
-    if x.find("=") > 0 ]
+    hardware_info = subprocess.check_output(hardware_cmd, stderr=subprocess.PIPE)
+    args = [ x.split("=",1) for x in 
+        open(cmdline_file, "r").readline().strip().split()
+        if x.find("=") > 0 ]
 
-kv = {}
-for k,v in args:
-    kv[k] = v
-mac_address = kv['macaddr']
-dsc_base = kv['url']
-dsc_hw_new = dsc_base + "/admin/hardware/new"
-dsc_hw_list = dsc_base + "/admin/hardware/view"
-dsc_hw_edit= dsc_base + "/admin/hardware/edit/{0}"
+    kv = {}
+    for k,v in args:
+        kv[k] = v
+    mac_address = kv['macaddr']
+    dsc_base = kv['url']
+d   sc_hw_new = dsc_base + "/admin/hardware/new"
+    dsc_hw_list = dsc_base + "/admin/hardware/view"
+    dsc_hw_edit= dsc_base + "/admin/hardware/edit/{0}"
 
-data = json.dumps(dict(mac_address=mac_address, 
+    data = json.dumps(dict(mac_address=mac_address, 
                        hardware_info=hardware_info))
-req = urllib2.Request(dsc_hw_new, data, {"Content-Type": "application/json"})
+    req = urllib2.Request(dsc_hw_new, data, {"Content-Type": "application/json"})
 
-try:
-    urllib2.urlopen(req)
-except urllib2.HTTPError, e:
-    if e.code == 500:
+    try:
+        urllib2.urlopen(req)
+    except urllib2.HTTPError, e:
+        if e.code == 500:
         #check to see if mac currently exists.  If so, update mac with new hwinfo
-        try:
-            req = urllib2.Request(dsc_hw_list)
-            req.add_header("Accept", "application/json")
-            hw = json.loads(urllib2.urlopen(req).read())['hardware']
-            record = [s for s in hw if s["mac_address"] == mac_address][0]
-            req = urllib2.Request(dsc_hw_edit.format(record['id']),
+            try:
+                req = urllib2.Request(dsc_hw_list)
+                req.add_header("Accept", "application/json")
+                hw = json.loads(urllib2.urlopen(req).read())['hardware']
+                record = [s for s in hw if s["mac_address"] == mac_address][0]
+                req = urllib2.Request(dsc_hw_edit.format(record['id']),
                                   data, {"Content-Type": "application/json"})
-            urllib2.urlopen(req)
-        except:
-            traceback.print_exc(file=sys.stdout)
-            #Discovery has failed.  Reboot!
-            print "Discovery failed.  Rebooting."
-            #subprocess.call("/sbin/reboot")
+                urllib2.urlopen(req)
+            except:
+                traceback.print_exc(file=sys.stdout)
+                #Discovery has failed.  Reboot!
+                print "Discovery failed.  Rebooting."
+                #subprocess.call("/sbin/reboot")
--- logscraper.py
+++ logscraper.py
@@ -2,23 +2,48 @@
 
 import sys
 import socket
+import urllib2
 
-hostname = socket.gethostname()
+try:
+    import json
+except ImportError:
+    import simplejson as json
 
-if len(sys.argv) == 2:
-    log_file = open(sys.argv[1],"r")
-else:
-    log_file = sys.stdin
+def main():
+    hostname = socket.gethostname()
 
-while 1:
-    line = log_file.readline()
-    if not line:
-        break
-    line = line[line.find(hostname):]
-    (syslog_stuff, msg) = [ x.strip() for x in line.split(":",1) ]
-    if msg.find("DHCPDISCOVER") >= 0 and msg.find("ignored") > 0:
-        print msg.split()[1]
-    if msg.find("DHCPDISCOVER") >= 0 and msg.find("no address available") > 0:
-        parts = msg.split()
-        idx = parts.index("no") - 1
-        print msg.split()[idx]
+    if len(sys.argv) == 2:
+        log_file = open(sys.argv[1],"r")
+    else:
+        log_file = sys.stdin
+    
+    macs = set("")
+    
+    while 1:
+        line = log_file.readline()
+        if not line:
+            break
+        mac = ""
+        line = line[line.find(hostname):]
+        (syslog_stuff, msg) = [ x.strip() for x in line.split(":",1) ]
+        if msg.find("DHCPDISCOVER") >= 0 and msg.find("ignored") > 0:
+            mac = msg.split()[1]
+        if msg.find("DHCPDISCOVER") >= 0 and msg.find("no address available") > 0:
+            parts = msg.split()
+            idx = parts.index("no") - 1
+            mac = msg.split()[idx]
+        if not mac in macs:
+            macs.add(mac)
+            register_mac(mac)
+
+def register_mac(mac,url="http://localhost:5000/admin/hardware/new"):
+    data = json.dumps(dict(mac_address=mac))
+    req = urllib2.Request(url, data, {"Content-Type": "application/json"})
+    try:
+        urllib2.urlopen(req)
+    except urllib2.HTTPError, e:
+        #ignore error
+        pass
+                
+if __name__=="__main__":
+    main()
