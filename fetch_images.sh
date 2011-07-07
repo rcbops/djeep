c2ae065d382228988480aca26360600c5ada2c64
Will Kelly <the.william.kelly@gmail.com>
2011-07-07 16:54:07 -0500

    Made fetch_images.sh pull down pxelinux and maverick files

--- fetch_images.sh
+++ fetch_images.sh
@@ -4,3 +4,8 @@
 # and initrds from somewhere
 
 # I should also be made into a manage.py command
+
+wget http://c222662.r62.cf1.rackcdn.com/syslinux-needful.tar.gz -qO- |
+    tar xzC tftproot
+wget http://c222662.r62.cf1.rackcdn.com/maverick.tar.gz -qO- |
+    tar xzC tftproot
