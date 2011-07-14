e477f69021af82add6a1b0d7b876cca4c5f7081a
Anthony Young <sleepsonthefloor@gmail.com>
2011-07-13 17:57:45 -0700

    add ability to clone configs from one cluster to another

--- rolemapper/admin.py
+++ rolemapper/admin.py
@@ -1,5 +1,7 @@
 from django import forms
+from django import shortcuts
 from django.contrib import admin
+from django.contrib import messages
 from django.db import models as django_models
 
 from djeep.rolemapper import models
@@ -15,6 +17,35 @@ class ConfigAdmin(admin.ModelAdmin):
   formfield_overrides = {
       django_models.TextField: {'widget': forms.TextInput(attrs={'size': 100})}
   }
+
+  def clone_config(modeladmin, request, queryset):
+    clone_to = request.POST.get('clone_to')
+    if not clone_to:
+      messages.error(request, "Please specifiy a Cluster Name")
+    else:
+      cluster = models.Cluster.objects.filter(short_name__exact=clone_to)
+      if cluster:
+        cluster = cluster[0]
+        for config in queryset:
+          previous = models.Config.objects.filter(cluster__exact=cluster.id,
+                                                  key__exact=config.key)
+          if not previous:
+            config.id = None
+            config.cluster = cluster
+            config.save()
+            messages.info(request, "Config %s added to %s(%s)" %
+                                   (config.key, cluster.short_name, cluster.id))
+          else:
+            messages.info(request, "Config %s already exists for %s(%s)" %
+                                   (config.key, cluster.short_name, cluster.id))
+        return shortcuts.redirect(request.build_absolute_uri() +
+                                  ("?cluster__id__exact=%s" % cluster.id))
+      else:
+        messages.error(request, "Cluster '%s' not found" % clone_to)
+      pass
+
+  clone_config.short_description = 'Clone To Other Cluster'
+  actions = [clone_config]
 
 admin.site.register(models.Config, ConfigAdmin)
 
@@ -69,7 +100,6 @@ class RoleAdmin(admin.ModelAdmin):
   list_editable = ('name', )
 
 admin.site.register(models.Role, RoleAdmin)
-
 
 class RoleMapAdmin(admin.ModelAdmin):
   list_display = ('id', 'role', 'name')
