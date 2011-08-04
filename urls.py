1da44e111251e19ba07e5301f5229d9391ed3873
termie <github@anarkystic.com>
2011-08-04 16:17:13 -0700

    update settings to serve media files

--- urls.py
+++ urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from django.conf import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -30,4 +31,10 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+
+    # Handling for static media
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+    }),
 )
