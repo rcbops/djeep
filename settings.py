61dc9cf807f13cc9ccfb575110280a43647565d2
termie <github@anarkystic.com>
2011-08-08 11:46:51 -0700

    remove extra comma

--- settings.py
+++ settings.py
@@ -49,7 +49,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'media'),
+MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
