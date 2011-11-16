372b5d9985189e9e8b11ae3d64ee227319596148
Will Kelly <the.william.kelly@gmail.com>
2011-11-16 15:04:28 -0600

    All directories that require djeep writes live under local/ now

--- fetch_images.sh
+++ fetch_images.sh
@@ -6,6 +6,6 @@
 # I should also be made into a manage.py command
 
 wget http://c752981.r81.cf2.rackcdn.com/syslinux-needful.tar.gz -qO- |
-    tar xzC tftproot
+    tar xzC local/tftproot
 wget http://c752981.r81.cf2.rackcdn.com/maverick.tar.gz -qO- |
-    tar xzC tftproot
+    tar xzC local/tftproot
