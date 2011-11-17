3c7c4df41097131022b51e267839e21fdb2afd3c
Will Kelly <the.william.kelly@gmail.com>
2011-11-17 16:42:35 -0600

    Fixed missing files issue with djeep restructure

---
+++ local/etc/puppet/node_classes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import json
+import os
+
+ROOT = os.path.dirname(__file__)
+
+
+def main(hostname):
+  hostfile = os.path.join(ROOT, 'hosts', hostname)
+  host_info = json.load(open(hostfile))
+  cluster = host_info['options'].get('cluster')
+  clusterfile = os.path.join(ROOT, 'clusters', cluster)
+  cluster_info = json.load(open(clusterfile))
+
+  options = cluster_info.get('options', {}).copy()
+  options.update(host_info.get('options', {}))
+
+  classes = cluster_info.get('classes', [])[:]
+  classes.extend(host_info.get('classes', []))
+
+  print json.dumps({'parameters': options, 'classes': list(set(classes))},
+                   indent=2)
+
+
+if __name__ == '__main__':
+  import sys
+  main(sys.argv[1])
