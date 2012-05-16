95c4207015a21d49cc413a92194784c3da18e896
galstrom21 <jshepher@rackspace.com>
2012-05-15 20:51:39 -0500

    adding auto-creation of rsa ssh keys

--- templates/post_script/debian.sh
+++ templates/post_script/debian.sh
@@ -7,6 +7,8 @@ if [ -s /root/install.sh ]; then
     sed -i /etc/rc.local -e 's_exit 0_/root/install.sh_'
 fi
 
+ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa
+
 echo "{{host.hostname}}" > /etc/hostname
 curl http://{{site.webservice_host}}:{{site.webservice_port}}/api/host/{{host.id}} -H "Content-type: application/json" -d '{"local_boot": 1}' -X "PUT"
 curl http://{{site.webservice_host}}:{{site.webservice_port}}/api/host/{{host.id}}/puppet_sig -X "DELETE"
