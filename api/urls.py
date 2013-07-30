08c06ea23f5c53852831b4ca5931da9c71a1cb75
galstrom21 <jshepher@rackspace.com>
2013-07-30 09:54:55 -0500

    Adding API for reading kicktargets
    * GET /api/kick - returns all kicktargets
    * GET /api/kick/<id> - returns kickatarget <id>

--- api/urls.py
+++ api/urls.py
@@ -8,6 +8,9 @@ from djeep.api import handlers
 host_handler = resource.Resource(handlers.HostHandler)
 host_handler = csrf.csrf_exempt(host_handler)
 
+kick_handler = resource.Resource(handlers.KickTargetHandler)
+kick_handler = csrf.csrf_exempt(kick_handler)
+
 puppet_handler = resource.Resource(handlers.PuppetHandler)
 puppet_handler = csrf.csrf_exempt(puppet_handler)
 
@@ -19,4 +22,6 @@ urlpatterns = patterns('',
     url(r'^host/(?P<id>\d+)/puppet_sig$', puppet_handler),
     url(r'^host/(?P<id>\d+)$', host_handler),
     url(r'^host/', host_handler),
+    url(r'^kick/(?P<id>\d+)$', kick_handler),
+    url(r'^kick/', kick_handler),
 )
