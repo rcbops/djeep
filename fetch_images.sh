d0f3a3ece359ecdae010a04b58b7785d0ba85ed5
galstrom21 <jshepher@rackspace.com>
2013-09-26 14:02:49 -0500

    Adding initial support for CentOS-6.4 as a kick target

--- fetch_images.sh
+++ fetch_images.sh
@@ -1,7 +1,45 @@
 #!/bin/bash
 
-# You should be able to run me to fetch all the appropriate kernels
-# and initrds from somewhere
+function fetch_ubuntu {
+  local DIRNAME="ubuntu"
+  local SUFFIX="amd64"
+  local urlbase="http://archive.ubuntu.com"
+  local images=( maverick natty oneiric precise )
+  local files=( initrd.gz linux )
+
+  mkdir ${DIRNAME}
+
+  pushd ${DIRNAME}
+  for image in ${images[@]}; do
+      echo "Grabbing files for Ubuntu-${image}"
+      mkdir "${image}-${SUFFIX}";
+      # cp preseed.txt ../../../templates/preseed/${image}-${SUFFIX}-preseed.txt;
+      for file in ${files[@]}; do
+          wget "${urlbase}/ubuntu/dists/${image}/main/installer-${SUFFIX}/current/images/netboot/ubuntu-installer/${SUFFIX}/${file}" -q -P ${image}-${SUFFIX};
+      done;
+  done;
+  popd
+}
+
+function fetch_centos {
+  local DIRNAME="centos"
+  local SUFFIX="x86_64"
+  local urlbase="http://mirror.rackspace.com"
+  local images=( 6.4 )
+  local files=( initrd.img vmlinuz )
+
+  mkdir ${DIRNAME}
+
+  pushd ${DIRNAME}
+  for image in ${images[@]}; do
+      echo "Grabbing files for CentOS-${image}"
+      mkdir "${image}-${SUFFIX}";
+      for file in ${files[@]}; do
+          wget "${urlbase}/CentOS/${image}/os/${SUFFIX}/images/pxeboot/${file}" -q -P ${image}-${SUFFIX};
+      done;
+  done;
+  popd
+}
 
 # I should also be made into a manage.py command
 OLDPWD="$PWD"
@@ -15,17 +53,8 @@ ln -s ../../syslinux-6.01/bios/core/pxelinux.0
 ln -s ../../syslinux-6.01/bios/com32/menu/menu.c32
 ln -s ../../syslinux-6.01/bios/com32/mboot/mboot.c32
 ln -s ../../syslinux-6.01/bios/com32/chain/chain.c32
-mkdir ubuntu
-cd ubuntu
 
-images=( maverick natty oneiric precise)
-for image in ${images[@]}; do
-    echo "Grabbing files for ${image}"
-    mkdir "${image}-amd64";
-    # cp preseed.txt ../../../templates/preseed/${image}-amd64-preseed.txt;
-    files=( initrd.gz linux )
-    for file in ${files[@]}; do
-        wget "http://archive.ubuntu.com/ubuntu/dists/${image}/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/${file}" -q -P ${image}-amd64;
-    done;
-done;
+fetch_ubuntu
+fetch_centos
+
 cd $OLDPWD
